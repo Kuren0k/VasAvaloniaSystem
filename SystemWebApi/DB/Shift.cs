56c79663a79bd4ffc700340c00cc6394213049a6
kuren0k <nikita.57t@gmail.com>
2026-02-03 14:00:51 +1000

    yo

---
+++ SystemWebApi/DB/Shift.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SystemWebApi.DB;
+
+public partial class Shift
+{
+    public int Id { get; set; }
+
+    public int EmployeeId { get; set; }
+
+    public DateTime StartDateTime { get; set; }
+
+    public DateTime EndDateTime { get; set; }
+
+    public string Description { get; set; } = null!;
+
+    public virtual Employee Employee { get; set; } = null!;
+}
